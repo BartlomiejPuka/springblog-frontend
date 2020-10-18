8465cae65adcc7dd19ab72115816a57ea5a40e9a
Bartlomiej Puka <bartlomiej.puka@netsoftware.pl>
2020-10-18 18:25:00 +0200

    - added login page and connected login form with backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import {RegisterPayload} from './register-payload';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {Observable} from 'rxjs';
+import {LoginPayload} from './login-payload';
+import {JwtAutResponse} from './jwt-aut-response';
+import {map} from 'rxjs/operators';
+import {LocalStorageService} from "ngx-webstorage";
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +13,18 @@ import {Observable} from "rxjs";
 export class AuthService {
   private url = 'http://localhost:8080/api/auth/';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private localStorageService: LocalStorageService) { }
 
   register(registerPayload: RegisterPayload): Observable<any>{
     return this.httpClient.post(this.url + 'signup', registerPayload);
   }
 
 
+  login(loginPayload: LoginPayload): Observable<boolean>{
+    return this.httpClient.post<JwtAutResponse>(this.url + 'login', loginPayload).pipe(map(data => {
+      this.localStorageService.store('authentication', data.authenticationToken);
+      this.localStorageService.store('username', data.username);
+      return true;
+    }));
+  }
 }
